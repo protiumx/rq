5d77692474b691705cb2c904b0b497213a98f0ba
Brian Mayo <ioprotium@gmail.com>
2022-05-22 20:15:05 +0200

    feat(rq-core): first implementation of HttpClient

--- rq-core/src/lib.rs
+++ rq-core/src/lib.rs
@@ -2,12 +2,4 @@
 extern crate pest_derive;
 
 pub mod parser;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod request;
---
+++ rq-core/src/request.rs
@@ -0,0 +1,49 @@
+extern crate reqwest;
+
+use reqwest::header;
+
+use crate::parser::{HttpMethod, HttpRequest};
+use std::time::Duration;
+
+pub struct HttpClient {
+    pub client: reqwest::blocking::Client,
+}
+
+impl HttpClient {
+    pub fn new() -> Self {
+        let mut headers = header::HeaderMap::new();
+        headers.insert(
+            header::CONTENT_TYPE,
+            header::HeaderValue::from_static("application/json"),
+        );
+        headers.insert(
+            header::ACCEPT,
+            header::HeaderValue::from_static("application/json"),
+        );
+
+        let client = reqwest::blocking::Client::builder()
+            .timeout(Duration::from_secs(10))
+            .default_headers(headers)
+            .no_gzip()
+            .build()
+            .unwrap();
+        HttpClient { client }
+    }
+
+    pub fn execute(&self, req: &HttpRequest) -> Result<(), Box<dyn std::error::Error>> {
+        let request = match req.method {
+            HttpMethod::Get => self.client.get(&req.url),
+            HttpMethod::Post => self.client.post(&req.url),
+            HttpMethod::Put => self.client.post(&req.url),
+            HttpMethod::Delete => self.client.post(&req.url),
+        };
+
+        let headers: header::HeaderMap = (&req.headers).try_into()?;
+
+        let res = request.headers(headers).send()?;
+
+        println!("{}\n", res.text()?);
+
+        Ok(())
+    }
+}

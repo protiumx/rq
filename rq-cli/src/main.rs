cf5d16785d61cc2c46369f7787a34c4ad953743f
Brian Mayo <ioprotium@gmail.com>
2022-05-22 20:15:37 +0200

    feat(rq-cli): select only one request and send it

--- rq-cli/src/main.rs
+++ rq-cli/src/main.rs
@@ -1,17 +1,22 @@
 use rq_core::parser::parse;
+use rq_core::request::HttpClient;
 
 use std::env;
 use std::fs;
 
-use inquire::MultiSelect;
+use inquire::Select;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     let file_content = fs::read_to_string(args[1].as_str())?;
     let http_file = parse(&file_content).unwrap();
-    println!("file:\n{}", http_file);
 
-    let ans = MultiSelect::new("Select requests to execute:", http_file.requests).prompt();
-    println!("selected {:?}", ans);
+    let request = Select::new("Select requests to execute:", http_file.requests)
+        .prompt()
+        .unwrap();
+
+    let client = HttpClient::new();
+    client.execute(&request)?;
+
     Ok(())
 }

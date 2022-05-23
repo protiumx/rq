1251923fef4b0aadac278f0bcfccd808479c6e2d
Brian Mayo <ioprotium@gmail.com>
2022-05-23 19:16:48 +0200

    feat(rq-cli): added error message when no file provided

--- rq-cli/src/main.rs
+++ rq-cli/src/main.rs
@@ -8,6 +8,11 @@ use inquire::Select;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("No files provided");
+        return Ok(());
+    }
+
     let file_content = fs::read_to_string(args[1].as_str())?;
     let http_file = parse(&file_content).unwrap();
 

8967458484a95ee20f6e2e866d31d89a2e108521
Brian Mayo <ioprotium@gmail.com>
2022-06-03 01:34:57 +0200

    refactor: remove comments; add exit code on error

--- rq-cli/src/main.rs
+++ rq-cli/src/main.rs
@@ -9,25 +9,14 @@ use std::fs;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        println!("No files provided");
-        return Ok(());
+        eprintln!("error: no files provided");
+        std::process::exit(1);
     }
 
     let file_content = fs::read_to_string(args[1].as_str())?;
-    let http_file = parse(&file_content).unwrap();
+    let http_file = parse(&file_content)?;
 
-    // clear screen
-    //println!("\r\x1b[2J\r\x1b[H");
-
-    //for req in http_file.requests {
-    //println!("  {}\n", req.print());
-    //}
-    //println!("\x1b[H");
-    //print!("\x1b[32m>");
-
-    //io::stdout().flush().unwrap();
-
-    app::run(http_file).await.unwrap();
+    app::run(http_file).await?;
 
     std::process::exit(0)
 }

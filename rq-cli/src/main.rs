c836aac867a30013ae2d8c491341edd77c1cd76d
TheRealLorenz <lollobellina@gmail.com>
2023-10-13 15:22:51 +0200

    Better args fetching

--- rq-cli/src/main.rs
+++ rq-cli/src/main.rs
@@ -10,13 +10,13 @@ use std::fs;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        eprintln!("error: no files provided");
-        std::process::exit(1);
-    }
-
-    let file_path = args[1].to_string();
+    let file_path = match env::args().nth(1) {
+        Some(file_path) => file_path,
+        None => {
+            eprintln!("error: no files provided");
+            std::process::exit(1);
+        }
+    };
     let file_content = fs::read_to_string(&file_path)?;
 
     let http_file = match parse(&file_content) {

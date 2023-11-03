319ffab1195f326b7be582021bd79578afe3c200
TheRealLorenz <lollobellina@gmail.com>
2023-11-03 16:38:15 +0100

    Lazily init single reqwest::Client

--- rq-core/src/request.rs
+++ rq-core/src/request.rs
@@ -1,5 +1,6 @@
 extern crate reqwest;
 
+use once_cell::sync::Lazy;
 pub use reqwest::StatusCode;
 use reqwest::{
     header::{self, HeaderMap},
@@ -9,24 +10,13 @@ use reqwest::{
 use crate::parser::HttpRequest;
 use std::{str::FromStr, time::Duration};
 
-fn new_client() -> Client {
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_static("application/json"),
-    );
-    headers.insert(
-        header::ACCEPT,
-        header::HeaderValue::from_static("application/json"),
-    );
-
+static CLIENT: Lazy<Client> = Lazy::new(|| {
     Client::builder()
         .timeout(Duration::from_secs(10))
-        .default_headers(headers)
         .no_gzip()
         .build()
         .unwrap()
-}
+});
 
 #[derive(Clone)]
 pub struct Response {
@@ -58,8 +48,7 @@ impl Response {
 type RequestResult = Result<Response, Box<dyn std::error::Error + Send + Sync>>;
 
 pub async fn execute(req: &HttpRequest) -> RequestResult {
-    let request =
-        new_client().request(Method::from_str(req.method.to_string().as_str())?, &req.url);
+    let request = CLIENT.request(Method::from_str(req.method.to_string().as_str())?, &req.url);
 
     let headers: header::HeaderMap = (req.headers()).try_into()?;
 
